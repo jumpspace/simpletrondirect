889ff410af061b6fba4a2bc2c349a2d0c52b5a51
Patrick Wong <studioapartment@hotmail.com>
2025-01-08 01:00:26 -0800

    Add controls to form. Create Javascript file to capture SML code and store in a JSON file.

---
+++ js/createJsonFile.js
@@ -0,0 +1,35 @@
+// createJsonFile.js
+let smlCode = [];
+
+function updateAddrLoc() {
+    'use strict';
+
+
+}
+
+function insertSml() {
+    'use strict';
+
+    smlCode.push({
+        "addr": document.getElementById('addrloc').value,
+        "code": document.getElementById('instcode').value
+    });
+}
+
+function serializeJson() {
+    //TODO: convert Javaacript array into JSON document.
+
+}
+
+function init() {
+    'use strict';
+
+    if (document && document.getElementById) {
+        let codeForm = document.getElementById('codeform');
+        document.getElementById('addrloc').value = '';
+
+        codeForm.onsubmit = serializeJson
+    }
+}
+
+window.onload = init;
